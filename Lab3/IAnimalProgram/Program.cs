85cefc057613c7b23c084daf58408714cd99f0bb
Ashleen Sidhu <100872112+AshleenSidhu@users.noreply.github.com>
2024-06-16 22:04:50 -0600

    Added another program

---
+++ Lab3/IAnimalProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IAnimalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
